1d19e0be1fc4abf960d842a995c022c7dd739682
Shishir Gurung <gurungsh@gmail.com>
2019-09-06 13:34:30 -0500

    Updates

---
+++ precache-manifest.40978409d56830241d82da879dd6e39c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd42160b279c432ef6f6c9ba6a14f5ef",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "11604f251faaa75ea0ec",
+    "url": "/todolist/static/css/main.57cd0de3.chunk.css"
+  },
+  {
+    "revision": "359dbd98e2450631943c",
+    "url": "/todolist/static/js/2.8df7f1ec.chunk.js"
+  },
+  {
+    "revision": "11604f251faaa75ea0ec",
+    "url": "/todolist/static/js/main.55c80de4.chunk.js"
+  },
+  {
+    "revision": "494ea65bfa3a7b566acf",
+    "url": "/todolist/static/js/runtime~main.b0077857.js"
+  }
+]);
